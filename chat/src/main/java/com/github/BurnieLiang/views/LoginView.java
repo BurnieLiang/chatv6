a84fd83a7f37c8d87022c7882c1a9c86348c9201
liangboning <952735135@qq.com>
2020-09-11 17:41:20 +0800

    init project

---
+++ chat/src/main/java/com/github/BurnieLiang/views/LoginView.java
@@ -0,0 +1,20 @@
+package com.github.BurnieLiang.views;
+
+import com.github.BurnieLiang.bootjavafx.AbstractFxmlView;
+import com.github.BurnieLiang.bootjavafx.FXMLView;
+
+//import javax.annotation.PostConstruct;
+
+/**
+ * @author Boning Liang
+ * @date 2020-09-11 16:27:59
+ */
+@FXMLView("/views/login.fxml")
+public class LoginView extends AbstractFxmlView {
+
+//    @PostConstruct
+//    protected void initUI() throws Exception {
+//        System.out.println("init ui");
+//    }
+
+}
