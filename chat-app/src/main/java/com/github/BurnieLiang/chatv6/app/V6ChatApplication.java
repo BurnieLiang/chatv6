8e6cfd5671568977282248cfc7bc2f17f66d9098
liangboning <952735135@qq.com>
2020-09-14 10:44:27 +0800

    update

--- chat-app/src/main/java/com/github/BurnieLiang/chatv6/app/V6ChatApplication.java
+++ chat-app/src/main/java/com/github/BurnieLiang/chatv6/app/V6ChatApplication.java
@@ -11,24 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {"com.github.BurnieLiang.chatv6.*"})
 public class V6ChatApplication extends AbstractJavaFxApplicationSupport {
 
-//    @Override
-//    public void start(Stage stage) throws Exception {
-//        ResourceBundle bundle = ResourceBundle.getBundle("strings", Locale.getDefault());
-//
-//        Parent root = FXMLLoader.load(getClass().getResource("/views/login.fxml"), bundle);
-//        stage.setTitle("Hello World");
-//        stage.setScene(new Scene(root));
-//        stage.show();
-//    }
-
-
-//    public static void main(String[] args) {
-//        Locale.setDefault(Locale.SIMPLIFIED_CHINESE);
-//        launch(args);
-//    }
-
-
-
     public static void main(String[] args) {
         launch(V6ChatApplication.class, LoginView.class, args);
     }
